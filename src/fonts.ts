58974d6b22236e1f93f23087c04a52073f6ffa0c
Alexander Vasilev <46332577+rayonnant@users.noreply.github.com>
2026-03-29 23:01:09 +0300

    chore: remove unuzed font inter

--- src/fonts.ts
+++ src/fonts.ts
@@ -1,8 +1,3 @@
 import '@fontsource/manrope/500.css';
 import '@fontsource/manrope/600.css';
 import '@fontsource/manrope/700.css';
-
-import '@fontsource/inter/400.css';
-import '@fontsource/inter/500.css';
-import '@fontsource/inter/700.css';
-import '@fontsource/inter/900.css';
