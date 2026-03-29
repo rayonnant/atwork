4aff13a0bef6fff121f103a10f4ffe78e5a9cc77
Alexander Vasilev <46332577+rayonnant@users.noreply.github.com>
2026-03-29 22:11:23 +0300

    feat: add styles for NotFoundPage

--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,3 +1,18 @@
+import { Link } from 'react-router-dom';
+import styles from './NotFoundPage.module.scss';
+
 export const NotFoundPage: React.FC = () => {
-  return <h2>Not Found Page</h2>;
+  return (
+    <main className={styles['not-found']}>
+      <div className='wrapper'>
+        <div className={styles['not-found__content']}>
+          <span className={styles['not-found__code']}>404</span>
+          <h2 className={styles['not-found__title']}>Страница не найдена</h2>
+          <Link to='/' className={styles['not-found__button']}>
+            Вернуться
+          </Link>
+        </div>
+      </div>
+    </main>
+  );
 };
